725f3f61e63ba50eb93e4e929216ce602378adef
surrealshinjokun <sglaw1111@gmail.com>
2024-03-07 04:15:05 -0600

    Fixed major bug

--- plugin.js
+++ plugin.js
@@ -1,6 +1,16 @@
-function main() {
-    try {
-		context.executeAction("pausetoggle", {});
+function main() {  
+	try {
+		var pluginStartTime = new Date().getTime();
+		var tickDispose = context.subscribe("interval.tick", function () {
+			var firstTickTime = new Date().getTime();
+			var timeBewteenTicks = firstTickTime - pluginStartTime;
+			if (timeBewteenTicks >= 500) {
+				tickDispose.dispose();
+			} else {
+				context.executeAction("pausetoggle", {}),
+				tickDispose.dispose();
+			}
+		});
 	} catch (error) {
 		console.log(error);
 	}
@@ -8,7 +18,7 @@ function main() {
 
 registerPlugin({
     name: 'OpenRCT2-AutomaticallyPause',
-    version: '1.1',
+    version: '1.2',
     authors: ['Ken'],
     type: 'remote',
     licence: 'Unlicense',
