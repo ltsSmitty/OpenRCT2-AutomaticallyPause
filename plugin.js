290ab44d5645fe59426600d7d6e9b46232716259
Smitty Penman <smitty.penman@gmail.com>
2024-03-07 04:24:07 -0500

    check for time between ticks to know whether to toggle

--- plugin.js
+++ plugin.js
@@ -1,17 +1,27 @@
 function main() {
-    try {
-		context.executeAction("pausetoggle", {});
-	} catch (error) {
-		console.log(error);
-	}
+  var pluginStartTime = new Date().getTime();
+  var tickDispose = context.subscribe("interval.tick", function () {
+    var firstTickTime = new Date().getTime();
+    var timeBewteenTicks = firstTickTime - pluginStartTime;
+    if (timeBewteenTicks >= 500) {
+      console.log(
+        "gamefile was already paused on load, so no need to toggle; disposing of tick listener."
+      );
+      tickDispose.dispose();
+    } else {
+      console.log("not paused, so activating toggle."),
+        context.executeAction("pausetoggle", {}),
+        tickDispose.dispose();
+    }
+  });
 }
 
 registerPlugin({
-    name: 'OpenRCT2-AutomaticallyPause',
-    version: '1.1',
-    authors: ['Ken'],
-    type: 'remote',
-    licence: 'Unlicense',
-    targetApiVersion: 7,
-    main: main
+  name: "OpenRCT2-AutomaticallyPause",
+  version: "1.1",
+  authors: ["Ken"],
+  type: "remote",
+  licence: "Unlicense",
+  targetApiVersion: 7,
+  main: main,
 });
